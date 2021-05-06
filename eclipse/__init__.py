60adaeaabfc07775da6e6c8b1496773b94f288d7
Stuart Mumford <stuart@cadair.com>
2021-05-06 15:36:26 +0100

    some more docs

--- eclipse/__init__.py
+++ eclipse/__init__.py
@@ -1,4 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from pkg_resources import resource_filename
 
+from .version import __version__
+
 SAMPLE_PHOTO = resource_filename('eclipse.sample_photos', 'total_solar_eclipse2017.jpg')
+
